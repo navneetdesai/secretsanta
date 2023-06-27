d7245fdb9f8dfd8a693da6b710ebaa310cb8a443
Navneet Desai <navneetdesai44@gmail.com>
2023-06-27 14:53:09 +0530

    Add hooks

--- server/controllers/participant.js
+++ server/controllers/participant.js
@@ -1,23 +1,22 @@
-const Participant = require('../models/participant');
-const assignSecretSantas = require('../utils/random-assignment');
-const validator = require('validator');
-
+const Participant = require("../models/participant");
+const assignSecretSantas = require("../utils/random-assignment");
+const validator = require("validator");
 
 const participantController = {
   createParticipant: async (req, res) => {
     try {
       const { name, email } = req.body;
-        if (!name || !email) {
-            return res.status(400).json({ error: 'Name and email are required' });
-        }
-        if (!validator.isEmail(email)) {
-            return res.status(400).json({ error: 'Email is invalid' });
-        }
+      if (!name || !email) {
+        return res.status(400).json({ error: "Name and email are required" });
+      }
+      if (!validator.isEmail(email)) {
+        return res.status(400).json({ error: "Email is invalid" });
+      }
 
       const participant = await Participant.create({ name, email });
       res.status(201).json(participant);
     } catch (error) {
-      res.status(500).json({ error: 'Failed to create participant'  + error});
+      res.status(500).json({ error: "Failed to create participant" + error });
     }
   },
 
@@ -26,7 +25,7 @@ const participantController = {
       const participants = await Participant.find();
       res.status(200).json(participants);
     } catch (error) {
-      res.status(500).json({ error: 'Failed to retrieve participants' });
+      res.status(500).json({ error: "Failed to retrieve participants" });
     }
   },
 
@@ -37,10 +36,10 @@ const participantController = {
       if (participant) {
         res.status(200).json(participant);
       } else {
-        res.status(404).json({ error: 'Participant not found' });
+        res.status(404).json({ error: "Participant not found" });
       }
     } catch (error) {
-      res.status(500).json({ error: 'Failed to retrieve participant' });
+      res.status(500).json({ error: "Failed to retrieve participant" });
     }
   },
 
@@ -48,29 +47,32 @@ const participantController = {
     try {
       const participants = await Participant.find();
       const assignments = assignSecretSantas(participants);
-    //   console.log(assignments);
+      //   console.log(assignments);
 
-  
       for (const assignment of assignments) {
         const { participantId, secretSantaId } = assignment;
-        let participant = await Participant.findById(participantId);
+        const participant = await Participant.findById(participantId);
         if (!participant) {
-            return res.status(404).json({ error: 'Participant not found' });
+          return res.status(404).json({ error: "Participant not found" });
         }
-        console.log(participant, secretSantaId);
 
-        await Participant.updateOne({ _id: participantId }, { $set: { assignedRecipient: secretSantaId } });
+        await Participant.updateOne(
+          { _id: participantId },
+          { $set: { assignedRecipient: secretSantaId } }
+        );
         await participant.save();
       }
-  
-      res.status(200).json({ message: 'Secret Santas assigned successfully' });
+
+      res.status(200).json({ message: "Secret Santas assigned successfully" });
     } catch (error) {
-      res.status(500).json({ error: `Failed to assign Secret Santas.`, details: `${error}` });
+      res
+        .status(500)
+        .json({
+          error: "Failed to assign Secret Santas.",
+          details: `${error}`,
+        });
     }
   },
-  
-
-
 };
 
 module.exports = participantController;
